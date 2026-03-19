d84dbc46c42ff0fa6b65a540b6044021b0105c89
Planetlarg1 <sc232ah@leeds.ac.uk>
2026-03-19 21:18:59 +0000

    Endpoints for creating, updating, and deleting observations added

--- app/main.py
+++ app/main.py
@@ -281,7 +281,256 @@ def delete_station(station_id: int, db: Session = Depends(get_db)):
             detail=f"Cannot delete station {station.name} while it has dependencies."
         )
 
-    # Delete station
+    # Delete station 
     db.delete(station)
     db.commit()
     return {"message": f"Station {station.name} has been successfully deleted."}
+
+
+#######################
+# OBSERVATIONS ROUTES #
+#######################
+
+# POST Observations
+@app.post("/observations", response_model=schemas.ObservationCreate, status_code=status.HTTP_201_CREATED)
+def create_observation(observation: schemas.ObservationCreate, db: Session = Depends(get_db)):
+    # Check station exists
+    station = db.query(models.Station).filter(models.Station.id == observation.station_id).first()
+    if not station:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Station with id {observation.station_id} not found."
+        )
+    
+    # Sanitise Month
+    if observation.month < 1 or observation.month > 12:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Invalid month index {observation.month}."
+        )
+    
+    # Prevent Duplicate station/year/month combination
+    existing_observation = (
+        db.query(models.Observation)
+        .filter(
+            models.Observation.station_id == observation.station_id,
+            models.Observation.year == observation.year,
+            models.Observation.month == observation.month
+        )
+        .first()
+    )
+    if existing_observation:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The observation for {observation.month}/{observation.year} from station {observation.station_id} already exists."
+        )
+    
+    # Add new observation
+    new_observation = models.Observation(
+        station_id = observation.station_id,
+        year = observation.year,
+        month = observation.month,
+        tmax = observation.tmax,
+        tmin = observation.tmin,
+        af = observation.af,
+        rain = observation.rain,
+        sun = observation.sun
+    )
+
+    db.add(new_observation)
+    db.commit()
+    db.refresh(new_observation)
+    return new_observation
+
+
+# GET Observations (by city, by start/end year/month)
+@app.get("/observations", response_model=List[schemas.ObservationResponse])
+def get_observations(
+        # Filters
+        city_id: Optional[int] = None,
+        start_year: Optional[int] = None,
+        start_month: Optional[int] = None,
+        end_year: Optional[int] = None,
+        end_month: Optional[int] = None,
+        db: Session = Depends(get_db)
+):
+    # If month is given then year is required
+    if ((start_month is not None and start_year is None)
+        or (end_month is not None and end_year is None)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot filter by month without providing a year."
+        )
+    
+    # Validate month ranges
+    if ((start_month is not None and (start_month < 1 or start_month > 12)) 
+        or (end_month is not None and (end_month < 1 or end_month > 12))):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid month index."
+        )
+    
+    # Check start before end
+    if start_year is not None and end_year is not None:
+        if (start_year > end_year):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Start date must come before end date."
+            )
+        elif ((start_year == end_year) 
+            and (start_month is not None and end_month is not None)
+            and (start_month > end_month)):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Start date must come before end date."
+            )
+    
+    # Check city exists
+    if city_id is not None:
+        city = db.query(models.City).filter(models.City.id == city_id).first()
+        if not city:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"City with id {city_id} not found"
+            )
+        
+    query = db.query(models.Observation).join(models.Station)
+
+    # Filter by City
+    if city_id is not None:
+        query = query.filter(models.Station.city_id == city_id)
+
+    # Filter by start date
+    if start_year is not None:
+        if start_month is not None:
+            query = query.filter(
+                (models.Observation.year > start_year) |
+                (
+                    (models.Observation.year == start_year) &
+                    (models.Observation.month >= start_month)
+                )
+            )
+        else:
+            query = query.filter(models.Observation.year >= start_year)
+
+    # Filter by end date
+    if end_year is not None:
+        if end_month is not None:
+            query = query.filter(
+                (models.Observation.year < end_year) |
+                (
+                    (models.Observation.year == end_year) &
+                    (models.Observation.month <= end_month)
+                )
+            )
+        else:
+            query = query.filter(models.Observation.year <= end_year)
+
+    # Run query and return
+    results = query.all()
+    return results
+
+
+# GET Observation BY ID
+@app.get("/observations/{observation_id}", response_model=schemas.ObservationResponse)
+def get_observation(observation_id: int, db: Session = Depends(get_db)):
+    observation = (
+        db.query(models.Observation)
+        .filter(models.Observation.id == observation_id)
+        .first()
+    )
+
+    # Check exists
+    if not observation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Observation with id {observation_id} not found."
+        )
+    
+    return observation
+
+
+# PUT Observation BY ID
+@app.put("/observations/{observation_id}", response_model=schemas.ObservationUpdate)
+def update_observation(observation_id: int, observation_update: schemas.ObservationUpdate, db: Session = Depends(get_db)):
+    observation = (
+        db.query(models.Observation)
+        .filter(models.Observation.id == observation_id)
+        .first()
+    )
+
+    # Check exists
+    if not observation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Observation with id {observation_id} not found."
+        )
+    
+    # Check that the station exists
+    station = db.query(models.Station).filter(models.Station.id == observation_update.station_id).first()
+    if not station:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Station with id {observation_update.station_id} not found."
+        )
+    
+    # Validate month
+    if observation_update.month < 1 or observation_update.month > 12:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Month index {observation_update.month} invalid."
+        )
+    
+    # Prevent year/month/station duplicate
+    existing_observation = (
+        db.query(models.Observation)
+        .filter(
+            models.Observation.station_id == observation_update.station_id,
+            models.Observation.year == observation_update.year,
+            models.Observation.month == observation_update.month,
+            models.Observation.id != observation_id
+        )
+        .first()
+    )
+    if existing_observation:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The observation for {observation_update.month}/{observation_update.year} from station {observation_update.station_id} already exists."
+        )
+    
+    # Create new observation and add to DB
+    observation.station_id = observation_update.station_id
+    observation.year = observation_update.year
+    observation.month = observation_update.month
+    observation.tmax = observation_update.tmax
+    observation.tmin = observation_update.tmin
+    observation.af = observation_update.af
+    observation.rain = observation_update.rain
+    observation.sun = observation_update.sun
+
+    db.commit()
+    db.refresh(observation)
+    return observation
+
+
+# DELETE Observation By ID
+@app.delete("/observations/{observation_id}", status_code=status.HTTP_200_OK)
+def delete_observation(observation_id: int, db: Session = Depends(get_db)):
+    observation = (
+        db.query(models.Observation)
+        .filter(models.Observation.id == observation_id)
+        .first()
+    )
+
+    # Check if exists
+    if not observation:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Observation with id {observation_id} not found."
+        )
+    
+    # Delete observation
+    db.delete(observation)
+    db.commit()
+
+    return {"message": f"Observation with id {observation_id} has been successfully deleted."}
--- app/schemas.py
+++ app/schemas.py
@@ -14,7 +14,8 @@ class CityResponse(CityBase):
     id: int
 
     class Config:
-        from_attribute = True
+        from_attributes = True
+
 
 # STATIONS
 class StationBase(BaseModel):
@@ -33,4 +34,41 @@ class StationResponse(StationBase):
     id: int
 
     class Config:
-        from_attribute = True
+        from_attributes = True
+
+
+# OBSERVATIONS
+class ObservationBase(BaseModel):
+    station_id: int
+    year: int
+    month: int
+    tmax: float | None # Not required
+    tmin: float | None # Not required
+    af: float | None # Not required
+    rain: float | None # Not required
+    sun: float | None # Not required
+
+class ObservationCreate(ObservationBase):
+    pass
+
+class ObservationUpdate(ObservationBase):
+    pass
+
+class ObservationResponse(ObservationBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+# USERS
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        from_attributes = True
